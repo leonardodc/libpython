d8f7a5194ffc8c6b8a08ac81fb0be527a2c152be
Leonardo Demeterko <leonardo_foz@hotmail.com>
2023-03-13 04:26:15 -0300

    criando enviador de e-mail com test - part2

--- libpythonavatar/tests/test_spam/test_enviador_de_email.py
+++ libpythonavatar/tests/test_spam/test_enviador_de_email.py
@@ -1,20 +1,35 @@
-from pytest import mark
-from libpythonavatar.spam.enviador_de_email import Enviador
+import pytest
+from libpythonavatar.spam.enviador_de_email import Enviador, EmailInvalido
 
 def test_criar_enviador_de_email():
     enviador = Enviador()
     assert enviador is not None
 
-@mark.parametrize(
-    'destinatario',
+@pytest.mark.parametrize(
+    'remetente',
     ['leonardo_foz@hotmail.com', 'foo@bar.com.br']
 )
-def test_remetente(destinatario):
+def test_remetente(remetente):
     enviador = Enviador()
     resultado = enviador.enviar(
-        destinatario,
+        remetente,
         'leonardodemeterko@gmail.com',
         'Curso Python',
         'Estudos aplicados em 2023'
     )
-    assert destinatario in resultado
+    assert remetente in resultado
+
+@pytest.mark.parametrize(
+    'remetente',
+    ['', 'renzo']
+)
+def test_remetente_Invalido(remetente):
+    enviador = Enviador()
+    with pytest.raises(EmailInvalido):
+        enviador.enviar(
+            remetente,
+            'leonardodemeterko@gmail.com',
+            'Curso Python',
+            'Estudos aplicados em 2023'
+        )
+    
