b7bffbfa2d4789e5a8ae5815d8723eba6d6be9d2
Leonardo Demeterko <leonardo_foz@hotmail.com>
2023-03-19 09:08:34 -0300

    Módulo como Objeto - Isolamento de Imports - Biblioteca Pytest-Mock

---
+++ libpythonavatar/tests/test_spam/test_github_api.py
@@ -0,0 +1,24 @@
+from unittest.mock import Mock
+import pytest
+from libpythonavatar import github_api
+
+
+@pytest.fixture
+def avatar_url(mocker):
+    resp_mock = Mock()
+    url = 'https://avatars.githubusercontent.com/u/7226004?v=4'
+    resp_mock.json.return_value = {
+        'login': 'leonardodc', 'id': 7226004,
+        'avatar_url': url,
+    }
+    get_mock = mocker.patch('libpythonavatar.github_api.requests.get')
+    get_mock.return_value = resp_mock
+    return url
+
+def test_buscar_avatar(avatar_url):
+    url = github_api.buscar_avatar('leonardodc')
+    assert avatar_url == url
+
+def test_buscar_avatar_integracao():
+    url = github_api.buscar_avatar('leonardodc')
+    assert 'https://avatars.githubusercontent.com/u/7226004?v=4' == url
